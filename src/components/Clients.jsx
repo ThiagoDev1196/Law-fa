3cb869f86d29ced4ff913df263cdafa1b9dba222
Thiago <thiagomarcial1109@gmail.com>
2026-03-22 16:28:56 -0300

    Added clients section

---
+++ src/components/Clients.jsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { clientsData } from "../data/clients.js";
+
+function Clients() {
+
+  const [currentPage, setCurrentPage] = useState(0); // Estado de página actual
+  const [itemsPerPage, setItemsPerPage] = useState(1); // Por defecto, 3 reseñas por página
+
+  useEffect(() => {
+      const updateItemsPerPage = () => {
+          if (window.innerWidth <= 767) {
+              setItemsPerPage(1); 
+          }
+          
+          else if (window.innerWidth >= 767 && window.innerWidth < 1280) {
+              setItemsPerPage(2); 
+          }
+          else {
+              setItemsPerPage(3);
+          }
+      };
+
+      updateItemsPerPage(); 
+      window.addEventListener("resize", updateItemsPerPage); 
+
+      return () => window.removeEventListener("resize", updateItemsPerPage);
+  }, []);
+
+
+  const totalPages = Math.ceil(clientsData.length / itemsPerPage); 
+
+  const startIndex = currentPage * itemsPerPage; 
+  const displayedClients = clientsData.slice(startIndex, startIndex + itemsPerPage); 
+
+
+  const handleNext = () => {
+    setCurrentPage((prev) => (prev + 1) % totalPages); 
+
+  };
+
+  const handlePrev = () => {
+    setCurrentPage((prev) => (prev - 1 + totalPages) % totalPages);
+  };
+
+  
+    return (
+        <section className="clients-container bg-clients flex flex-col items-center justify-center p-4 gap-4">
+            
+            <h3 className="text-4xl font-bold text-center py-5 sm:text-5xl lg:text-6xl">What our clients said</h3>
+
+            <div className="clients-items flex flex-row items-center md:gap-8 justify-center">
+               {displayedClients.map((client) => (
+                 <div key={client.id} className="flex flex-col gap-4 items-center justify-center p-4 border-gradient
+                 w-70 h-110 sm:w-80 sm:h-120 lg:w-90 lg:h-130
+                 ">
+                    <img className="w-38 h-38 rounded-full object-cover border-gold border-2 sm:w-48 sm:h-48 lg:w-58 lg:h-58" src={client.picture} alt={client.name} />
+                    <p className="text-lg font-semibold sm:text-xl lg:text-2xl">{client.name}</p>
+                    <p className="text-white text-md sm:text-lg"><i>"{client.review}"</i></p>
+                 </div>
+                ))}
+            </div>
+
+      <div className="flex justify-center gap-4 mt-6 mb-11 md:text-lg xl:text-xl">
+        <button onClick={handlePrev} className="cursor-pointer px-4 py-2 md:px-6 bg-gold rounded-lg text-black hover:bg-darkgold transition-all ease-in-out">Previous</button>
+        <button onClick={handleNext} className="cursor-pointer px-4 py-2 md:px-6 bg-gold rounded-lg text-black hover:bg-darkgold transition-all ease-in-out">Next</button>
+      </div>
+
+        </section>
+    )
+}
+
+export default Clients
---
+++ src/data/clients.js
@@ -0,0 +1,87 @@
+export const clientsData = [
+    {
+        id: 1,
+        picture: '/clients-pics/client1.png',
+        name: 'Jonathan Joestar',
+        review: "The firm provided exceptional guidance throughout my legal process. What I valued most was their ability to explain complex legal terms in a way that made sense for my situation."
+    },
+    
+    {
+        id: 2,
+        picture: '/clients-pics/client2.jpg',
+        name: 'Abigail Martson',
+        review: "The firm's strategic insight and empathetic approach truly set them apart. I felt supported and well-informed throughout. Truly professional and effective counsel."
+    },
+
+    {
+        id: 3,
+        picture: '/clients-pics/client3.jpg',
+        name: 'Lucy Steel',
+        review: 'When I was facing a complicated civil dispute, this website connected me with the perfect attorney for my case. Thanks to their dedicated representation, we were able to win the case and achieve a positive outcome.'
+    },
+
+    {
+        id: 4,
+        picture: '/clients-pics/client4.jpg',
+        name: 'Michael Mercury',
+        review:'Outstanding experience with this firm. Their honesty and seriousness from the first meeting gave me peace of mind, and we ended up winning a complex case thanks to their efficiency.'
+    },
+
+    {
+        id: 5,
+        picture: '/clients-pics/client5.jpg',
+        name: 'Padme Admidala',
+        review: 'The legal team was incredibly kind and professional throughout my entire process. Their strategic approach is unmatched, and they secured a favorable outcome much faster than I expected.'
+    },
+
+    {
+        id: 6,
+        picture: '/clients-pics/client6.jpg',
+        name: 'Luis Alberto',
+        review: 'Finally a law firm that prioritizes integrity and clear communication. They handled my civil matter with total transparency and won the case with impressive expertise.'
+    },
+
+    {
+        id: 7,
+        picture: '/clients-pics/client7.jpg',
+        name: 'Calíope',
+        review: 'I was impressed by their efficiency and dedication to my defense. They are true professionals who take every detail seriously, leading us to a total victory in court.'
+    },
+
+    {
+        id: 8,
+        picture: '/clients-pics/client8.jpg',
+        name: 'Erwin Smith',
+        review: 'Extremely helpful and knowledgeable attorneys who actually care about their clients. Their honest advice and commitment to my success resulted in a perfect resolution for my legal dispute.'
+    },
+
+    {
+        id: 9,
+        picture: '/clients-pics/client9.jpg',
+        name: 'Edward Elric',
+        review: 'Top-tier service defined by seriousness and a high success rate. They managed my case with such precision and kindness that I felt confident from start to finish.'
+    },
+
+    {
+        id: 10,
+        picture: '/clients-pics/client10.jpg',
+        name: 'Aubrey',
+        review: 'Highly efficient and result-oriented legal representation. They won my case against all odds, proving their status as one of the most reliable and honest firms in the field.'
+    },
+
+    {
+        id: 11,
+        picture: '/clients-pics/client11.jpg',
+        name: 'Midoriya',
+        review: "I was impressed by the firm's efficiency and transparent communication. They handled my case with extreme seriousness and achieved a favorable outcome faster than I ever imagined."
+    },
+
+    {
+        id: 12,
+        picture: '/clients-pics/client12.jpg',
+        name: 'Rosé',
+        review: 'Truly a professional team that combines kindness with a high success rate. They were honest about my options from day one and fought hard to win my legal dispute in court.'
+    },
+
+    
+]
