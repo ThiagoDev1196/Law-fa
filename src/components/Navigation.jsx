b20ecf45fdae07d40c3b5a33d1658d82165134ed
Thiago <thiagomarcial1109@gmail.com>
2026-03-26 15:49:59 -0300

    Added GitHub pages config

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -16,7 +16,7 @@ function Nav() {
              <button className="xl:hidden text-xl " onClick={toggleMenu} id="menu-open" aria-label="Open navegation menu">☰</button>
 
             <div className="icon-container-mobile">
-              <img src="/favicon.svg" className="w-10 h-10"/>
+              <img src="/law-fa/favicon.svg" className="w-10 h-10"/>
             </div>
           </div>
 
@@ -29,7 +29,7 @@ function Nav() {
 
         <ul className={`nav-ul ${isOpen ? 'flex-col' : 'hidden'} w-full md:flex md:flex-row md:justify-end p-3 md:text-lg`} id="nav">
             <div className="icon-container hidden md:flex md:mr-auto">
-              <img src="/favicon.svg" className="w-10 h-10 md:w-12 md:h-12"/>
+              <img src="/law-fa/favicon.svg" className="w-10 h-10 md:w-12 md:h-12"/>
             </div>
 
           <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu}  className="hover:text-darkgold transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#">Home</a></li>
